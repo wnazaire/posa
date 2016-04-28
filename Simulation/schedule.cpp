67ae3d7b80811908271723f07efd38ae587f3e75
odriscollb <bodrisco@fau.edu>
2016-04-28 19:01:56 -0400

    Create schedule.cpp

---
+++ Simulation/schedule.cpp
@@ -0,0 +1,22 @@
+#include "schedule.h"
+
+Schedule::Schedule()
+{
+}
+
+void Schedule::add(Appointment *)
+{
+}
+
+void Schedule::viewUsersAppointments(User *)
+{
+}
+
+void Schedule::viewAll(User *)
+{
+}
+
+vector<Appointment*> Schedule::getSchedule()
+{
+	return vector<Appointment*>();
+}
