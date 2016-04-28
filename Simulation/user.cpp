a188c4af3cd248592645aa7f5b6a8238d56dbae5
odriscollb <bodrisco@fau.edu>
2016-04-28 19:01:17 -0400

    Create user.cpp

---
+++ Simulation/user.cpp
@@ -0,0 +1,25 @@
+#include "user.h"
+
+User::User(string, string, string, string)
+{
+}
+
+string User::getPassword()
+{
+	return string();
+}
+
+string User::getName()
+{
+	return string();
+}
+
+string User::getUsername()
+{
+	return string();
+}
+
+string User::getPrivilege()
+{
+	return string();
+}
